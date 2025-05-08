8a8fba500aea92e37781f9fc534c5e8678a6670d
Nicole Jardin <nicole.jardin1@icloud.com>
2025-05-09 07:44:55 +0800

    added records cards

--- crm_website/views.py
+++ crm_website/views.py
@@ -59,3 +59,13 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # lookup
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.error(request, "You must be logged in to view this page.")
+        return redirect('index')
+
